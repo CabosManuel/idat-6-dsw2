2d7ac0983f2b3057ce9d4b194ca48ee43e77c0c5
CabosManuel <cabos.apolaya@gmail.com>
2020-11-01 20:36:49 -0500

    Tarea1, proyecto base creado

---
+++ workspace/T1AppRest/src/main/java/com/cabos/T1AppRestApplication.java
@@ -0,0 +1,13 @@
+package com.cabos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1AppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1AppRestApplication.class, args);
+	}
+
+}
---
+++ workspace/T1AppRest/src/test/java/com/cabos/T1AppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1AppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
