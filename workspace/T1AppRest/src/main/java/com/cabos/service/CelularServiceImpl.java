a47838f2ca15da528d2b5815b03ce806c8bd5958
CabosManuel <cabos.apolaya@gmail.com>
2020-11-03 23:39:01 -0500

    @Anotaciones, inyección y transacciones

--- workspace/T1AppRest/src/main/java/com/cabos/service/CelularServiceImpl.java
+++ workspace/T1AppRest/src/main/java/com/cabos/service/CelularServiceImpl.java
@@ -2,7 +2,9 @@ package com.cabos.service;
 
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cabos.model.Celular;
 import com.cabos.repository.CelularRepository;
@@ -10,29 +12,35 @@ import com.cabos.repository.CelularRepository;
 @Service
 public class CelularServiceImpl implements CelularService{
 
+	@Autowired
 	private CelularRepository repository;
 	
 	@Override
+	@Transactional
 	public void insert(Celular celular) {
 		repository.save(celular);
 	}
 
 	@Override
+	@Transactional
 	public void update(Celular celular) {
 		repository.save(celular);
 	}
 
 	@Override
+	@Transactional
 	public void delete(Integer idCelular) {
 		repository.deleteById(idCelular);
 	}
 
 	@Override
+	@Transactional(readOnly=true)
 	public Celular findById(Integer idCelular) {
 		return repository.findById(idCelular).orElse(null);
 	}
 
 	@Override
+	@Transactional(readOnly=true)
 	public Collection<Celular> findAll() {
 		return (Collection<Celular>)repository.findAll();
 	}
