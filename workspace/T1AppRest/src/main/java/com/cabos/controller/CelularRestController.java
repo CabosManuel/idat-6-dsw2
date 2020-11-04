fc714f1f5e4d780a3ec80c96f59cb511e64f0787
CabosManuel <cabos.apolaya@gmail.com>
2020-11-03 23:43:28 -0500

    @Mapping + eliminar

--- workspace/T1AppRest/src/main/java/com/cabos/controller/CelularRestController.java
+++ workspace/T1AppRest/src/main/java/com/cabos/controller/CelularRestController.java
@@ -5,9 +5,11 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,20 +34,23 @@ public class CelularRestController {
 		return new ResponseEntity<>(itemsCelular,HttpStatus.OK);
 	}
 	
+	@GetMapping("/buscar/{idCelular}")
 	public ResponseEntity<?> buscar(@PathVariable Integer idCelular){
 		Celular celularDb = celularService.findById(idCelular);
-		if(celularDb!=null)
-			return new ResponseEntity<Void>(HttpStatus.OK);
 		
-		return null;
+		if(celularDb!=null) 
+			return new ResponseEntity<>(celularDb,HttpStatus.OK);
+		
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 	
-	@PostMapping
+	@PostMapping("/agregar")
 	public ResponseEntity<?> agregar(@RequestBody Celular celular){
 		celularService.insert(celular);
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
 	
+	@PutMapping("/editar/{idCelular}")
 	public ResponseEntity<?> editar(@PathVariable Integer idCelular, @RequestBody Celular newCelular){
 		Celular oldCelular = celularService.findById(idCelular);
 		
@@ -53,11 +58,22 @@ public class CelularRestController {
 			oldCelular.setMarca(newCelular.getMarca());
 			oldCelular.setModelo(newCelular.getModelo());
 			oldCelular.setPrecio(newCelular.getPrecio());
-			oldCelular.setTamaño(newCelular.getTamaño());
+			oldCelular.setTamano(newCelular.getTamano());
 			oldCelular.setPrecio(newCelular.getPrecio());
 			
 			celularService.update(oldCelular);
 			return new ResponseEntity<>(oldCelular,HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
+	@DeleteMapping("/eliminar/{idCelular}")
+	public ResponseEntity<?> eliminar(@PathVariable Integer idCelular) {
+		
+		if(celularService.findById(idCelular)!=null) {
+			celularService.delete(idCelular);
+			return new ResponseEntity<Void>(HttpStatus.OK); 
 		}
 		
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
