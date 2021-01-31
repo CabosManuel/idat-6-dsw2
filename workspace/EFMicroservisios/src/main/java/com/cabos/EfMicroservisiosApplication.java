559498e1df122839b442ff33a694432df79a35b6
CabosManuel <cabos.apolaya@gmail.com>
2021-01-30 23:07:06 -0500

    RestTemplate

--- workspace/EFMicroservisios/src/main/java/com/cabos/EfMicroservisiosApplication.java
+++ workspace/EFMicroservisios/src/main/java/com/cabos/EfMicroservisiosApplication.java
@@ -2,6 +2,8 @@ package com.cabos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EfMicroservisiosApplication {
@@ -10,4 +12,8 @@ public class EfMicroservisiosApplication {
 		SpringApplication.run(EfMicroservisiosApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
