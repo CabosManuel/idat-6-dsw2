067a4cb21bc9983e84f247e7e9afebf567cfb1ee
CabosManuel <cabos.apolaya@gmail.com>
2021-01-30 20:32:19 -0500

    Nuevo proyecto para EF de Microservicios

---
+++ workspace/EFMicroservisios/src/main/java/com/cabos/EfMicroservisiosApplication.java
@@ -0,0 +1,13 @@
+package com.cabos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfMicroservisiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfMicroservisiosApplication.class, args);
+	}
+
+}
---
+++ workspace/EFMicroservisios/src/test/java/com/cabos/EfMicroservisiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfMicroservisiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
