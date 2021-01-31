4df80740facc6d78c9788ba8e048895d409a6a07
CabosManuel <cabos.apolaya@gmail.com>
2021-01-30 23:06:47 -0500

    properties + Urls en paquete util

---
+++ workspace/EFMicroservisios/src/main/java/com/cabos/util/Url.java
@@ -0,0 +1,14 @@
+package com.cabos.util;
+
+public class Url {
+	public static final String SERVICIO = "http://localhost:8086/ec1/rest";
+	public static final String DLC = "/dlc";
+	public static final String VIDEOJUEGO = "/videojuego";
+	public static final String URL_DLC = SERVICIO + DLC;
+	public static final String URL_VIDEOJUEGO = SERVICIO + VIDEOJUEGO;
+	
+	public static final String LISTAR = "/listar";
+	public static final String AGREGAR = "/agregar";
+	public static final String EDITAR = "/editar";
+	public static final String ELIMINAR = "/eliminar";
+}
